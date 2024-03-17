01d00d9bbebb16690f2b31de304864e4b1e1049f
yaalyy <yaalyy@outlook.com>
2024-03-17 03:56:28 +0000

    added functions to control buil-in RGB

---
+++ buildInRGB.cpp
@@ -0,0 +1,25 @@
+#include "buildInRGB.h"
+#include <WiFiNINA.h>
+#include <utility/wifi_drv.h>
+
+
+void initBuildInRGB()
+{
+    WiFiDrv::pinMode(25, OUTPUT);    // configure red pin 
+    WiFiDrv::pinMode(26, OUTPUT);    // configure green pin
+    WiFiDrv::pinMode(27, OUTPUT);    // configure blue pin
+}
+
+void setBuildInRGB(int red, int green, int blue)
+{
+    WiFiDrv::analogWrite(25, red);
+    WiFiDrv::analogWrite(26, green);
+    WiFiDrv::analogWrite(27, blue);
+}
+
+void turnOffBuildInRGB()
+{
+    WiFiDrv::analogWrite(25, 0);
+    WiFiDrv::analogWrite(26, 0);
+    WiFiDrv::analogWrite(27, 0);
+}
---
+++ buildInRGB.h
@@ -0,0 +1,7 @@
+#ifndef BUILDINRGB_H
+#define BUILDINRGB_H
+
+void initBuildInRGB();
+void setBuildInRGB(int red, int green, int blue);
+void turnOffBuildInRGB();
+#endif
