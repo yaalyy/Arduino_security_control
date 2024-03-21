e36b880b8b1ac09bc772a96eea2677acb20653d4
yaalyy <yaalyy@outlook.com>
2024-03-21 05:16:22 +0000

    able to achieve basic function(timestamp is incorrect)

--- Arduino_security_control.ino
+++ Arduino_security_control.ino
@@ -6,10 +6,10 @@
 #include "passwordManager.h"
 
 
-const int ledPin = 1;
-const int buzzerPin = 2;
-const int buttonPin1 = 3;
-const int buttonPin2 = 4;
+const int ledPin = 0;
+const int buzzerPin = 1;
+const int buttonPin1 = 2;
+const int buttonPin2 = 3;
 const int buttonPin3 = 5;
 
 unsigned long prevMillis = 0;
@@ -18,6 +18,7 @@ const char* pwdFileName = "pwd.txt";
 const char* logFileName = "log.txt";
 const int pwdLength = 4;
 const int attemptLimit = 3;  // the maximum number of attempts to enter the passcode
+int attempts = 0;
 const long timeOffset = 0;  // time offset in seconds
 
 
@@ -26,7 +27,11 @@ Led led(ledPin);
 FileIOHandler fileIOHandler;
 PasswordManager pwdManager(timeOffset);
 
+bool inputtingPwd = false;
+std::string pwdInput = "";
 JsonDocument pwdDocument;
+JsonDocument logDocument;
+
 void setup() {
   // Initialize serial and wait for port to open:
   Serial.begin(9600);
@@ -47,7 +52,7 @@ void setup() {
   pwdManager.init(pwdFileName, logFileName);  // import password into the password manager
   fileIOHandler.init();
 
-  
+  setBuildInRGB(255,0,0);
   /*
      The following function allows you to obtain more information
      related to the state of network and IoT Cloud connection and errors
@@ -58,11 +63,32 @@ void setup() {
   setDebugMessageLevel(2);
   ArduinoCloud.printDebugInfo();
 
+  //buzzer.entryBeeping();
 }
 
 void loop() {
   ArduinoCloud.update();
-
+  //pwdManager.updateNTP();
+  if(ArduinoCloud.connected())
+  {
+    // set RGB to green
+    setBuildInRGB(0,255,0); 
+    
+  }
+  else
+  {
+    // set RGB to yellow
+    setBuildInRGB(255,255,0);
+  }
+  if(inputtingPwd)
+  {
+    digitalWrite(ledPin, HIGH);
+    inputPassword();
+  }
+  else
+  {
+    digitalWrite(ledPin, LOW);
+  }
   unsigned long currentMillis = millis();
   if(currentMillis - prevMillis >= interval)  // setting up the refresh interval
   {
@@ -74,7 +100,10 @@ void loop() {
     user_login_1 = pwdManager.getPassword("user_1").c_str();
     user_login_2 = pwdManager.getPassword("user_2").c_str();
 
-    
+    if(digitalRead(buttonPin3) == HIGH)
+    {
+      inputtingPwd = !inputtingPwd;
+    }
   }
   
 }
@@ -93,5 +122,61 @@ void onUserLogin1Change(){
 */
 void onUserLogin2Change(){
   pwdManager.changePassword("user_2", user_login_2.c_str());
+}
+
+void inputPassword()
+{
+  while((pwdInput.length() < pwdLength) && (inputtingPwd))
+  {
+    if(digitalRead(buttonPin1) == HIGH)
+    {
+      if(pwdInput.length() < pwdLength)
+      {
+        pwdInput += "0";
+        buzzer.buttonPressed();
+        delay(500);
+      }
+    }
+    if(digitalRead(buttonPin2) == HIGH)
+    {
+      if(pwdInput.length() < pwdLength)
+      {
+        pwdInput += "1";
+        buzzer.buttonPressed();
+        delay(500);
+      }
+    }
+    if((digitalRead(buttonPin3) == HIGH)&&(pwdInput.length() > 0))
+    {
+      inputtingPwd = !inputtingPwd;
+    }
+  }
+
+  if(pwdManager.verifyPassword(pwdInput.c_str()))
+  {
+    attempts = 0;
+    buzzer.entryBeeping();
+    pwdInput = "";
+    inputtingPwd = false;
+    std::string loginLog = pwdManager.getLatestLoginMsg();
+    deserializeJson(logDocument, loginLog);
+    if(logDocument["userName"] == "user_1")
+    {
+      user1_log = String(loginLog.c_str());
+    }
+    else if(logDocument["userName"] == "user_2")
+    {
+      user2_log = String(loginLog.c_str());
+    }
+    
+  }
+  else
+  {
+    attempts++;
+    buzzer.buzzerBeeping(1000, 1000);
+    pwdInput = "";
+    inputtingPwd = false;
+  }
+
 }
 
